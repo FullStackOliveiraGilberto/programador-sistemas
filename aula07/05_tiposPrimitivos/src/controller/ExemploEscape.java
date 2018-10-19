94aff9fc7775d9fb4f966183e29e05a7de7d7bcd
Gilberto Oliveira <skwcontadores@gmail.com>
2018-10-18 21:09:14 -0300

    programas Java
    Java básico

---
+++ aula07/05_tiposPrimitivos/src/controller/ExemploEscape.java
@@ -0,0 +1,17 @@
+package controller;
+
+public class ExemploEscape {
+
+	
+   public static void main(String[] args) {
+		
+		System.out.println("\t utilizando tab" );
+		System.out.println("\n n2x quebra linha" );
+		System.out.println("\\contra barra" );
+		
+		
+		
+	}
+	
+	
+}
---
+++ aula07/05_tiposPrimitivos/src/controller/ExemploFlutuante.java
@@ -0,0 +1,19 @@
+package controller;
+
+public class ExemploFlutuante {
+	
+public static void main(String[] args) {
+		
+		double  salario, aumento , novoSalario;
+		
+		salario = 12;
+		aumento = 3;
+		novoSalario = salario + (aumento * salario);;
+		
+				System.out.println("Novo Salario R$ =" + novoSalario );
+		
+		
+	}
+	
+
+}
