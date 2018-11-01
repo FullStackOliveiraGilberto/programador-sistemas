d8700c6d5a0ea7747cb297be026b9e11d69b59da
Gilberto Oliveira <skwcontadores@gmail.com>
2018-11-01 20:47:49 -0300

    Add files via upload

---
+++ aula15/15_VerdadeiroFalso/src/controller/Teste.java
@@ -0,0 +1,34 @@
+package controller;
+
+public class Teste {
+	public static void main(String[] args) {
+		boolean b =  false;
+		boolean a =  true;
+		if (a==b) {
+
+			System.out.println("iguais:");
+				
+		}else {
+
+			System.out.println("Diferentes:");
+			
+			b= true;
+			b = false;
+			int d =10 , f = 11;
+			boolean c = a == b;
+			
+			boolean w = d==f;
+			
+			if (d==f) {
+			System.out.println("valor::::"+w);
+			} else {
+				
+				System.out.println("valor diferente:");
+			}
+			
+			
+		}
+		
+	}
+
+}
